4d1d180e42fe8e3b18412f89f25343ff59d57ce7
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-04-11 22:54:45 +0200

    Test

--- client/ts/main.ts
+++ client/ts/main.ts
@@ -1,7 +1,13 @@
-console.log("TEst");
+window.onload = () => {
+  loadHTML();
+};
 
-const div: HTMLDivElement = document.createElement("div") as HTMLDivElement;
-div.className = "chatbox";
-div.style.border = "1px solid red";
-div.style.width = "80%";
-div.style.height = "80vh";
+const loadHTML = () => {
+  const div: HTMLDivElement = document.createElement("div") as HTMLDivElement;
+  div.className = "chatbox";
+  div.style.border = "1px solid red";
+  div.style.width = "80%";
+  div.style.height = "80vh";
+
+  document.body.appendChild(div);
+};
---
+++ dist/index.2e6d6d50.js
@@ -0,0 +1,13 @@
+window.onload = ()=>{
+    loadHTML();
+};
+const loadHTML = ()=>{
+    const div = document.createElement("div");
+    div.className = "chatbox";
+    div.style.border = "1px solid red";
+    div.style.width = "80%";
+    div.style.height = "80vh";
+    document.body.appendChild(div);
+};
+
+//# sourceMappingURL=index.2e6d6d50.js.map
