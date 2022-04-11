4280a6840e1b68b160a3b1b35eae0fbc59d29d8a
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2022-04-11 22:47:01 +0200

    fornt end test

--- client/ts/main.ts
+++ client/ts/main.ts
@@ -2,3 +2,6 @@ console.log("TEst");
 
 const div: HTMLDivElement = document.createElement("div") as HTMLDivElement;
 div.className = "chatbox";
+div.style.border = "1px solid red";
+div.style.width = "80%";
+div.style.height = "80vh";
